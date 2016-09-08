d5c0e2346a3c2d0d5fe85b2abd45485b1cf821dc
Maljl <maljl@kth.se>
2016-09-08 11:26:33 +0200

    Flyttat templets, lagt in testdata för quizen

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största 1slagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+
 def start(request):
-	return render(request, "quiz/start.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/start.html", context)
 
 def quiz(request):
-	return render(request, "quiz/quiz.html")
+	context = {
+		"quiz": quizzes[quiz_number -1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
