00ca7d49128781ac16a4b40575973f0b2aba7681
Maljl <maljl@kth.se>
2016-09-08 11:48:33 +0200

    Kopplat data till quiz-sidan

--- quiz/views.py
+++ quiz/views.py
@@ -23,9 +23,9 @@ def start(request):
 	}
 	return render(request, "quiz/start.html", context)
 
-def quiz(request):
+def quiz(request, quiz_number):
 	context = {
-		"quiz": quizzes[quiz_number -1],
+		"quiz": quizzes[int(quiz_number) -1],
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/quiz.html", context)
